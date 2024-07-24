511fed15bb0e7273001a042c8cd11542a33ce198
Syamala Nagendra Kumar Reddy <106234201+Nagendrakumarredd@users.noreply.github.com>
2024-07-24 18:14:05 +0530

    Create build.py

---
+++ build.py
@@ -0,0 +1,25 @@
+from llama_index.core import VectorStoreIndex, StorageContext
+from llama_index.vector_stores.weaviate import WeaviateVectorStore
+
+index_name = "SolutionWriteups"
+
+# Construct vector store
+vector_store = WeaviateVectorStore(
+    weaviate_client = client, 
+    index_name = index_name
+)
+
+# Set up the storage for the embeddings
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+# If an index with the same index name already exists within Weaviate, delete it
+if client.schema.exists(index_name):
+    client.schema.delete_class(index_name)
+
+# Setup the index
+# build VectorStoreIndex that takes care of chunking documents
+# and encoding chunks to embeddings for future retrieval
+index = VectorStoreIndex(
+    nodes,
+    storage_context = storage_context,
+)
